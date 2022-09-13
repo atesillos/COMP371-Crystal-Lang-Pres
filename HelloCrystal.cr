d5d8803b3955b257e9170653e67ce6845e0d71c8
Angel Tesillos <atesillos@luc.edu>
2022-09-13 17:40:34 -0500

    Changed HelloCrystal.cr

--- HelloCrystal.cr
+++ HelloCrystal.cr
@@ -1,5 +1,8 @@
 puts "Hello, class. This is the langauge Crytal!
 Firstly, there is no need to create a main file for this language.
 You can edit this file in the terminal using vim or emacs.
-You may use an IDE with extensions that support the language;
-I used IntelliJ IDEA with the normal Crystal Plug-in."
+You may use an IDE with extensions that support the language;"
+# Do not forget to comment out the line below and change it 
+# to say you used vim
+# puts "I used IntelliJ IDEA with the normal Crystal Plug-in."
+puts "I used vim in my Mac termincal to write the code!"
