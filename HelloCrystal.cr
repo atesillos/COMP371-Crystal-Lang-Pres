f144f7e23c8078074336842f793a04bf4ddb61a8
Angel Tesillos <90344721+atesillos@users.noreply.github.com>
2022-09-13 18:34:14 -0400

    Added HelloCrystal.cr file
    This has a simple puts for a quick description!

---
+++ HelloCrystal.cr
@@ -0,0 +1,5 @@
+puts "Hello, class. This is the langauge Crytal!
+Firstly, there is no need to create a main file for this language.
+You can edit this file in the terminal using vim or emacs.
+You may use an IDE with extensions that support the language;
+I used IntelliJ IDEA with the normal Crystal Plug-in."
