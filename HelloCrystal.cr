7dc4df26ed93408b4d3854d94149e13d96edf237
Angel Tesillos <atesillos@luc.edu>
2022-09-15 11:38:02 -0500

    Edited the 'HelloCrystal.cr' file.

--- HelloCrystal.cr
+++ HelloCrystal.cr
@@ -28,3 +28,21 @@ puts "#{numOne} + #{numTwo} = #{numOne + numTwo}"
 puts "#{numThree}"
 
 # We can change the variable type
+
+puts "Inside the code, we had smolString set to 'Hi!', but now it will equal the number 10, changing its type."
+
+smolString = 10
+
+puts "#{smolString} + #{numOne} = #{smolString + numOne}"
+
+# Methods
+
+# Cannot do:
+# x = 1
+# def add(y)
+# 	x + y
+# end
+# This will lead to an error since x is defined outside the method and not as a parameter
+def add (x,y)
+	x + y
+end
